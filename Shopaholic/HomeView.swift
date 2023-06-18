54e3da0fffeb356ff1e544175989aa741e27dd9b
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-18 13:10:07 -0500

    HomeView navbar

---
+++ Shopaholic/HomeView.swift
@@ -0,0 +1,73 @@
+//
+//  HomeView.swift
+//  Shopaholic
+//
+//  Created by Osaretin Uyigue on 6/18/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            NavBar()
+                .padding()
+                .background(Color.yellow)
+            
+            Spacer()
+        }
+    }
+}
+
+
+private struct NavBar: View {
+    var body: some View {
+        HStack {
+            NavItemButton(systemName: "line.horizontal.3.decrease")
+            Spacer()
+            NavItemButton(systemName: "magnifyingglass")
+            NavItemButton(systemName: "cart")
+                .overlay(alignment: .topTrailing) {
+                    cartRedDot
+                        .offset(x: 5, y: -5)
+                }
+        }
+    }
+    
+    
+    private var cartRedDot: some View {
+        Circle()
+            .fill(.red)
+            .frame(width: 15, height: 15)
+    }
+    
+}
+
+
+private struct NavItemButton: View {
+    var systemName: String
+    var action: (() -> Void)?
+    
+    init(systemName: String, action: (() -> Void)? = nil) {
+        self.systemName = systemName
+        self.action = action
+    }
+    
+    var body: some View {
+        Button {
+            action?()
+        } label: {
+            Image(systemName: systemName)
+                .font(.title)
+                .foregroundColor(.black)
+        }
+
+    }
+}
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- Shopaholic/ShopaholicApp.swift
+++ Shopaholic/ShopaholicApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ShopaholicApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
