0ca6246d3e155314a2da9d874c2ade1e8a7f0cf5
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-18 13:12:34 -0500

    NavBar refactor and shadow

--- Shopaholic/HomeView.swift
+++ Shopaholic/HomeView.swift
@@ -12,58 +12,15 @@ struct HomeView: View {
         VStack {
             NavBar()
                 .padding()
-                .background(Color.yellow)
+                .background(Color.white)
+                .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
             
             Spacer()
         }
+        .background(Color.black.opacity(0.05).ignoresSafeArea())
     }
 }
 
-
-private struct NavBar: View {
-    var body: some View {
-        HStack {
-            NavItemButton(systemName: "line.horizontal.3.decrease")
-            Spacer()
-            NavItemButton(systemName: "magnifyingglass")
-            NavItemButton(systemName: "cart")
-                .overlay(alignment: .topTrailing) {
-                    cartRedDot
-                        .offset(x: 5, y: -5)
-                }
-        }
-    }
-    
-    
-    private var cartRedDot: some View {
-        Circle()
-            .fill(.red)
-            .frame(width: 15, height: 15)
-    }
-    
-}
-
-
-private struct NavItemButton: View {
-    var systemName: String
-    var action: (() -> Void)?
-    
-    init(systemName: String, action: (() -> Void)? = nil) {
-        self.systemName = systemName
-        self.action = action
-    }
-    
-    var body: some View {
-        Button {
-            action?()
-        } label: {
-            Image(systemName: systemName)
-                .font(.title)
-                .foregroundColor(.black)
-        }
-
-    }
-}
 
 
 struct HomeView_Previews: PreviewProvider {
