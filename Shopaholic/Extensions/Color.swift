788e222c0a73429bcc9547935108ccc132875b99
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-18 13:25:32 -0500

    Import Colors, Assets and create model

---
+++ Shopaholic/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Shopaholic
+//
+//  Created by Osaretin Uyigue on 6/18/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = Theme()
+}
+
+struct Theme {
+    let bag1 = Color("bag1")
+    let bag2 = Color("bag2")
+    let bag3 = Color("bag3")
+    let bag4 = Color("bag4")
+    let bag5 = Color("bag5")
+    let bag6 = Color("bag6")
+}
---
+++ Shopaholic/Model/BagModel.swift
@@ -0,0 +1,26 @@
+//
+//  BagModel.swift
+//  Shopaholic
+//
+//  Created by Osaretin Uyigue on 6/18/23.
+//
+
+import Foundation
+
+struct BagModel: Identifiable {
+    var id = UUID().uuidString
+    var image: String
+    var title: String
+    var price: String
+    
+    static let bags = [
+        BagModel(image: "bag1", title: "Office Bag", price: "$234"),
+        BagModel(image: "bag5", title: "Stylus Bag", price: "$434"),
+        BagModel(image: "bag6", title: "Round Belt", price: "$134"),
+        BagModel(image: "bag2", title: "Belt Bag", price: "$294"),
+        BagModel(image: "bag3", title: "Hang Top", price: "$204"),
+        BagModel(image: "bag4", title: "Old Fashion", price: "$334")
+    ]
+}
+
+var scrollTabs = ["Hand Bag","Jewellery","Footwear","Dresses","Beauty"]
