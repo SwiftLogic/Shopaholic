f893e36ac9275b3514c04b036254d810ee258160
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-18 12:49:54 -0500

    Initial Commit

---
+++ Shopaholic/ShopaholicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopaholicApp.swift
+//  Shopaholic
+//
+//  Created by Osaretin Uyigue on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ShopaholicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
